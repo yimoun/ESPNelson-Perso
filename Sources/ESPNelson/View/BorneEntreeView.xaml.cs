2629c9122e3dc08dfc2b9932d8a76a602b8d139c
Nelson Junior Yimou Noubissi <2130331@etu.cchic.ca>
2025-02-25 23:10:11 -0500

    design EntryStation continue

--- Sources/ESPNelson/View/BorneEntreeView.xaml.cs
+++ Sources/ESPNelson/View/BorneEntreeView.xaml.cs
@@ -21,30 +21,51 @@ namespace ESPNelson.View
             StartFadeInAnimation(); // Déclencher l'animation initiale
         }
 
-        private void NavigationTabs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void BtnVisiteur_Click(object sender, RoutedEventArgs e)
         {
-            if (NavigationTabs.SelectedItem is TabItem selectedTab)
-            {
-                // Définir la nouvelle vue
-                Page newView = null;
-                switch (selectedTab.Header.ToString())
-                {
-                    case "Visiteur":
-                        newView = new VisiteurView();
-                        break;
-                    case "Abonné":
-                        newView = new AbonneView();
-                        break;
-                }
+            // Naviguer vers la vue "Visiteur" avec animation
+            NavigateToPage(new VisiteurView());
+        }
 
-                // Naviguer vers la nouvelle vue avec animation
-                if (newView != null && MainFrame != null) // Vérifier que MainFrame n'est pas null
-                {
-                    MainFrame.Navigate(newView); // Utiliser Frame.Navigate pour changer la vue
-                    StartFadeInAnimation(); // Déclencher l'animation
-                }
+        private void BtnAbonne_Click(object sender, RoutedEventArgs e)
+        {
+            // Naviguer vers la vue "Abonné" avec animation
+            NavigateToPage(new AbonneView());
+        }
+
+        private void NavigateToPage(Page newPage)
+        {
+            if (newPage != null && MainFrame != null) // Vérifier que MainFrame existe
+            {
+                MainFrame.Navigate(newPage);
+                StartFadeInAnimation(); // Déclencher l'animation après le changement de page
             }
         }
+
+        //private void NavigationTabs_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    if (NavigationTabs.SelectedItem is TabItem selectedTab)
+        //    {
+        //        // Définir la nouvelle vue
+        //        Page newView = null;
+        //        switch (selectedTab.Header.ToString())
+        //        {
+        //            case "Visiteur":
+        //                newView = new VisiteurView();
+        //                break;
+        //            case "Abonné":
+        //                newView = new AbonneView();
+        //                break;
+        //        }
+
+        //        // Naviguer vers la nouvelle vue avec animation
+        //        if (newView != null && MainFrame != null) // Vérifier que MainFrame n'est pas null
+        //        {
+        //            MainFrame.Navigate(newView); // Utiliser Frame.Navigate pour changer la vue
+        //            StartFadeInAnimation(); // Déclencher l'animation
+        //        }
+        //    }
+        //}
 
         private void StartFadeInAnimation()
         {
